3bd3a00da1fcbb8a131558c26272e81366a70c0e
maki0905 <k022g0059@g.neec.ac.jp>
2023-12-22 16:38:54 +0900

    リークチェッカー

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 ﻿#include <Windows.h>
 #include "GameManager.h"
+#include <dxgidebug.h>
+#pragma comment(lib, "dxguid.lib")
+
+struct ResourceLeakChecker {
+    ~ResourceLeakChecker() {
+        Microsoft::WRL::ComPtr<IDXGIDebug1> debug;
+        if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+            debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+            debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+            debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+        }
+    }
+};
+
+static ResourceLeakChecker leakChecker;
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	GameManager* gameManager_ = new GameManager;
-	gameManager_->Run();
+
+	GameManager* gameManager = new GameManager();
+	gameManager->Run();
+	delete gameManager;
 	return 0;
 }
